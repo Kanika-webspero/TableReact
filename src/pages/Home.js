695a8d6ee1a5b547bec40fcb8d640ab26622a5ae
Kanika webspero <kanika@webspero.com>
2021-11-11 18:19:22 +0530

    worked on useMemo, memo, remove bottom line of text field

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { TextField } from '@mui/material';
+import React, { useMemo, useState } from 'react';
 import Model from '../components/3DModel';
 import MainHeader from '../components/MainHeader';
 import { duplicateArray, numbers } from '../data/MainData';
@@ -6,7 +7,23 @@ import TemporaryDrawer from './Drawer';
 
 const Home = (props) => {
 
-    const [duplicateValues, setDupliacteValues] = useState(duplicateArray)
+    const [duplicateValues, setDupliacteValues] = useState(duplicateArray);
+      const [count, setCount] = useState(0);
+      const [item, setItem] = useState(10);
+    const [sameItem, setSameItem] = useState(0)
+    const [additionNum, setAdditionNum] = useState(0)
+    const [updateAdditionNum, setUpdateAdditionNum] = useState(0)
+    const [multipleInput, setMultipleInput] = useState(false)
+
+    const inputBox = () => {
+        setMultipleInput(true)
+    }
+      
+
+      const multiCountMemo = useMemo(function multiCount() {
+       console.log('multicount')
+        return count*5;
+      },[count])
 
     const removeDuplicate = () => {
        const dup = duplicateValues.filter((a, el) => {
@@ -23,6 +40,21 @@ const Home = (props) => {
         arrayNumbers.splice(3, 0, '0')
         let x = JSON.parse(JSON.stringify(arrayNumbers));
         setArrayNumbers(x)
+    }
+
+    // const clickToAdd = () => {
+    // }
+
+  
+
+    const addition = () => {
+        console.log('addition')
+        setAdditionNum(additionNum + 1)
+    }
+
+    const updateAddition = () => {
+        console.log('update addition')
+        setUpdateAdditionNum(additionNum);
     }
 
     return (
@@ -46,9 +78,38 @@ const Home = (props) => {
         //             <span key={index}>{nums}</span>
         //         ))}
         // </div>
+
+        
         <div className='homeContainer'>
         <div className='homeHeader'>
         <MainHeader setIsAuth={props.setIsAuth}/>
+      <div>
+      <div>Count: {count}</div>
+      <div>Item: {item}</div>
+      <div>SameItem: {sameItem}</div>
+        <div>{multiCountMemo}</div>
+      <button onClick={() => setCount(count + 1)}>Update count</button>
+      <button onClick={() => setItem(item + 1)}>Update item</button>
+        <button onClick={() => setSameItem(item)}>Update same item</button>
+
+      </div>
+      <div>
+      <div>{additionNum}</div>
+      <div>{updateAdditionNum}</div>
+      <button onClick={addition}>Addition</button>
+      <button onClick={updateAddition}>Update Addition</button>
+
+      </div>
+      <TextField  InputProps={{ disableUnderline: true }} id="standard-basic" label="Standard" variant="standard" />
+      <div>
+      
+      {multipleInput ? <div>
+        <input />
+        <input />
+          
+          </div> : <input onClick={inputBox} placeholder='name'/>}
+        
+      </div>
         </div>
         <Model />
         </div>
